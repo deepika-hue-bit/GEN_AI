f3729391ea9cf608e1e64535e92673dd03ca854e
deepika-hue-bit <deepikasuji50@gmail.com>
2025-03-29 11:13:51 +0530

    Update LAB6.py

--- LAB6.py
+++ LAB6.py
@@ -6,7 +6,7 @@ def main():
 
   input_text=st.text_input("Enter your text", "")
 
-  model=pipeline("sentiment-analysis",model="")
+  model=pipeline("sentiment-analysis",model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
 
   if st.button("Analyse"):
     result=model(input_text)
