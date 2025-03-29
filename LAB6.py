016ba1abe7d4d453651cc7d98da9a680de72b017
deepika-hue-bit <deepikasuji50@gmail.com>
2025-03-29 11:21:48 +0530

    Update LAB6.py

--- LAB6.py
+++ LAB6.py
@@ -4,12 +4,13 @@ from transformers import pipeline
 def main():
   st.title("Hugging face model demo")
 
-  input_text=st.text_input("Enter your text", "")
+  input_text=st.text_input("Enter your text", " ")
 
   model=pipeline("sentiment-analysis",model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
 
   if st.button("Analyse"):
     result=model(input_text)
     st.write("Prediction:", result[0]['label'],"|score:", result[0]['score'])
+    
 if __name__=="__main__":
   main()
